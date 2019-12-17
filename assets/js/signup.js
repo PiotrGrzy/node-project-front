659e17fdaa627189760eeffde16f023b079ff6b5
PiotrGrzy <48014334+PiotrGrzy@users.noreply.github.com>
2019-12-17 08:56:20 +0100

    add axios to signup

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,50 +1,44 @@
 const form = document.querySelector(".sign-up__form");
 
 const sendNewUser = async (name, email, password) => {
+  console.log(name, email, password);
 
-    console.log(name, email, password);
+  const data = JSON.stringify({
+    name: name,
+    email: email,
+    password: password
+  });
 
-    const data = JSON.stringify({
-        "name": name,
-        "email": email,
-        "password": password
+  try {
+    const response = await axios({
+      method: "post",
+      url: "http://node-api-estates.herokuapp.com/api/v1/users/signup",
+      headers: {
+        "content-type": "application/json"
+      },
+      data: data
     });
+    const user = response.data.data.user;
+    const token = response.data.token;
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("token", token);
+    alert("Dodano nowego użytkownika");
+    location.replace("../../index.html");
 
-    console.log(data);
-
-    var xhr = new XMLHttpRequest();
-
-    xhr.addEventListener("readystatechange", function () {
-        if (this.readyState === 4) {
-            console.log(JSON.parse(this.response));
-
-            const token = JSON.parse(this.response).token;
-            localStorage.setItem("token", token);
-
-            const user = JSON.parse(this.response).data.user;
-            localStorage.setItem("user", JSON.stringify(user));
-
-            console.log(token);
-            console.log(user);
-
-            alert("Jesteś zalogowany");
-            location.replace("../../index.html");
-        }
-    });
-
-    xhr.open("POST", "http://node-api-estates.herokuapp.com/api/v1/users/signup");
-    xhr.setRequestHeader("content-type", "application/json");
-
-    xhr.send(data);
-}
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+    alert("Coś poszło nie tak, spróbuj ponownie");
+  }
+};
 
 const onFormSubmit = async e => {
-    e.preventDefault();
-    const name = document.querySelector("#username").value;
-    const email = document.querySelector("#email").value;
-    const password = document.querySelector("#password").value;
+  e.preventDefault();
+  const name = document.querySelector("#username").value;
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
 
-    sendNewUser(name, email, password);
+  sendNewUser(name, email, password);
 };
 
 form.addEventListener("submit", onFormSubmit);
